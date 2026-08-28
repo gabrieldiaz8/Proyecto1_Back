73fa740234a2ef21d730049bc0cbb93e375db53b
Tomas Gambino <tomigambino21@gmail.com>
2026-08-28 15:07:48 -0300

    Correccion en CORS

--- proyecto/src/main.ts
+++ proyecto/src/main.ts
@@ -4,31 +4,27 @@ import { GlobalExceptionFilter } from './modules/common/filters/global-exception
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cors from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(cors({
-    origin: "https://gabrieldiaz8.github.io",
-    credentials: true, // si usás cookies/sesiones
-  }));
+
+  // Configuración nativa de CORS en NestJS
+  app.enableCors({
+    origin: 'https://gabrieldiaz8.github.io',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Convierte el cuerpo a la clase del DTO
       whitelist: true, // Elimina propiedades no declaradas en el DTO
       forbidNonWhitelisted: true, // Lanza error si se reciben propiedades no permitidas
-      /*
-      transformOptions: {
-        enableImplicitConversion: true,
-      },*/
     }),
   );
 
   const config = new DocumentBuilder()
     .setTitle('Gestión Base - Distribuidora')
-    .setDescription('La descripción de las  API  de la distribuidora')
+    .setDescription('La descripción de las API de la distribuidora')
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
@@ -39,13 +35,13 @@ async function bootstrap() {
 
   // Configurar filtro global de excepciones
   app.useGlobalFilters(new GlobalExceptionFilter());
-  // inspeccion de rutas
 
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-  // inspeccion de rutas
+
+  // Inspección de rutas
   const router = app.getHttpAdapter().getInstance();
   console.log(router._router?.stack);
 }

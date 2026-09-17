161571a2e79213492e904498ccdcbe7ceb820d54
Gabriel Diaz <gabidiaz4231@gmail.com>
2026-09-17 15:30:49 -0300

    feat(super-linea): crear entidad SuperLinea

---
+++ proyecto/src/modules/gestion-productos/super-linea/domain/entities/super-linea.entity.ts
@@ -0,0 +1,43 @@
+import { Linea } from "src/modules/gestion-productos/linea/domain/entities/linea.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+@Entity('super_linea')
+@Index(['denominacion', 'deletedAt'], { unique: true })
+export class SuperLineaEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255 })
+    denominacion: string;
+
+    @Column({ type: 'text', nullable: true })
+    observacion?: string;
+
+    /*
+    Comentado para que no de error por ahora
+    @OneToMany(() => Linea, (linea) => linea.superLinea)
+    lineas: Linea[]
+    */
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn({ nullable: true })
+    deletedAt?: Date;
+
+    @Column({ type: 'int', nullable: true })
+    usuarioCreatedId?: number; 
+
+    @Column({ type: 'int', nullable: true })
+    usuarioDeletedId?: number;
+    
+    @Column({ type: 'int', nullable: true })
+    usuarioUpdatedId?: number;
+    
+    @Column({ type: 'int', default: 0 })
+    sistema: number;
+}

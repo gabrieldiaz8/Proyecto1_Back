225ed250d8bdeda4343f3af24a49ba187687b298
LolitoGLezz <gonzalez.lorenzo2311@gmail.com>
2026-09-24 15:29:20 -0300

    fix(cr-002): unifica regex en DTOs para soportar caracteres de presentacion

--- proyecto/src/modules/gestion-productos/producto/dto/update-producto.dto.ts
+++ proyecto/src/modules/gestion-productos/producto/dto/update-producto.dto.ts
@@ -14,9 +14,8 @@ export class UpdateProductoDto extends PartialType(CreateProductoDto) {
   @IsString({ message: 'La denominación debe ser una cadena de texto.' }) // Valida que sea string
   @IsNotEmpty({ message: 'La denominación no puede estar vacía.' }) // Valida que no esté vacía
   @MaxLength(255, { message: 'La denominación no puede estar vacía.' })
-  @Matches(/^[A-Za-z0-9 áéíóúÁÉÍÓÚñÑ.\-/]+$/, {
-    message:
-      'La denominación solo puede contener letras, números, espacios, puntos, guiones y barras.',
+  @Matches(/^[\w áéíóúÁÉÍÓÚñÑ.\-/%]+$/, {
+    message: 'La denominación contiene caracteres inválidos ',
   })
   denominacion: string;
 
